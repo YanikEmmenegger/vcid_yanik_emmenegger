3265ac73b50ce144a3f24bf7cbb49f4f62917e0e
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-01 13:21:04 +0100

    environment configs (security)

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,34 +1,38 @@
-    import React from 'react';
-    import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-    import Layout from './components/Layout';
-    import {routes} from "./helpers/routes";
-    import axios from "axios"; // Assuming you have an About component
+import React from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Layout from './components/Layout';
+import {routes} from "./helpers/routes";
+import axios from "axios"; // Assuming you have an About component
 
 
-    const childRoutes = routes.map((route) => {
-        return {
-            path: "/app/"+route.path,
-            element: route.element,
-        };
-    });
+const childRoutes = routes.map((route) => {
+    return {
+        path: "/app/" + route.path,
+        element: route.element,
+    };
+});
 
 
-    // Define your routes here, including the layout and any child routes
-    const router = createBrowserRouter([
-        {
-            path: "/app/",
-            element: <Layout />,
-            children: childRoutes,
-        },
-    ]);
-    //set axios defaults withCredentials to true to send cookies and base url
-    axios.defaults.withCredentials = true;
-    if (process.env.REACT_APP_API_BASE_URL) axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
+// Define your routes here, including the layout and any child routes
+const router = createBrowserRouter([
+    {
+        path: "/app/",
+        element: <Layout/>,
+        children: childRoutes,
+    },
+]);
+//set axios defaults withCredentials to true to send cookies and base url
+axios.defaults.withCredentials = true;
+if (process.env.REACT_APP_API_BASE_URL) {
+    axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
+} else {
+    axios.defaults.baseURL = "https://"+window.location.host
+}
 
 
 
     function App() {
-        return <RouterProvider router={router} />;
+        return <RouterProvider router={router}/>;
     }
 
-    export default App;
+export default App;
