d9e08e734158644606e22d6b9efea772f291fdac
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-04 11:59:58 +0100

    boot.sh

--- service_files/boot.sh
+++ service_files/boot.sh
@@ -34,7 +34,7 @@ cd ..
 
 # Docker Compose zum Neustarten des Services verwenden
 echo "Starte den Service neu..."
-docker-compose down && docker-compose up -d
+docker compose down && docker compose up -d
 
 # Lösche alle nicht verwendeten Docker-Images
 echo "Lösche alle nicht verwendeten Docker-Images..."
