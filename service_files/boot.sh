20154aec15401629a1fb449f59ab2c4d6be13aba
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-04 11:58:57 +0100

    boot.sh

--- service_files/boot.sh
+++ service_files/boot.sh
@@ -2,8 +2,9 @@
 
 # Setzen Sie das erwartete Verzeichnis
 EXPECTED_DIR="/home/g1999emmenegger/vcid_yanik_emmenegger"
+SERVICE_FILES_DIR="service_files"
 
-# Aktuelles Verzeichnis prüfen
+# Aktuelles Verzeichnis prüfen und wechseln falls notwendig
 if [ "$(pwd)" != "$EXPECTED_DIR" ]; then
     cd $EXPECTED_DIR
 fi
@@ -14,6 +15,12 @@ git fetch --all
 git reset --hard origin/master
 git clean -fdx
 
+# Prüfen, ob sich etwas im Ordner "service_files" geändert hat
+if git diff --name-only HEAD@{1} | grep "$SERVICE_FILES_DIR/"; then
+    echo "Änderungen im '$SERVICE_FILES_DIR' Ordner erkannt. Kopiere Dateien nach '../'."
+    cp -r "$SERVICE_FILES_DIR/"* ../
+fi
+
 # Änderungen außerhalb des "react" Ordners prüfen
 if git status --porcelain | grep -v "^?? react/" | grep .; then
     echo "Änderungen außerhalb des 'react' Ordners erkannt. Erstelle ein neues Docker-Image."
@@ -22,12 +29,16 @@ else
     echo "Keine Änderungen außerhalb des 'react' Ordners erkannt. Überspringe das Erstellen eines neuen Docker-Images."
 fi
 
+# Wechseln in das Verzeichnis eine Ebene höher
+cd ..
+
 # Docker Compose zum Neustarten des Services verwenden
 echo "Starte den Service neu..."
-docker compose down && docker compose up -d
+docker-compose down && docker-compose up -d
 
 # Lösche alle nicht verwendeten Docker-Images
 echo "Lösche alle nicht verwendeten Docker-Images..."
 docker image prune -a --force
 
 echo "Vorgang abgeschlossen."
+
