1ba0fb1ce6c6042742fa0facebf74dd5767fd650
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-06 13:27:04 +0100

    autoupdater

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Stelle sicher, dass Skript als root ausgeführt wird
-
-
 # Update Package List
 sudo apt update
 
 # Installiere notwendige Pakete
-sudo apt install -y git docker docker-compose pip certbot python3-certbot-nginx
+sudo apt install -y git docker docker-compose python3-pip certbot
 
 # Mache Skripts ausführbar
 chmod +x ./boot.sh
@@ -17,12 +14,17 @@ chmod +x ./STARTER.sh
 # Frage den Benutzer nach der Domain
 read -p "Bitte geben Sie Ihre Domain ein: " DOMAIN
 
-# Führe den Certbot Befehl aus
-sudo certbot --nginx -d $DOMAIN
+# Stelle sicher, dass Port 80 verfügbar ist
+echo "Stelle sicher, dass Port 80 frei ist für die Certbot-Validierung"
+sudo fuser -k 80/tcp
 
-# Anpassen der nginx Konfiguration
-sudo sed -i "s|ssl_certificate /etc/letsencrypt/live/DEINE_DOMAIN/fullchain.pem;|ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;|" ./nginx/nginx.conf
-sudo sed -i "s|ssl_certificate_key /etc/letsencrypt/live/DEINE_DOMAIN/privkey.pem;|ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;|" ./nginx/nginx.conf
+# Führe den Certbot Befehl im Standalone-Modus aus
+sudo certbot certonly --standalone -d $DOMAIN
+
+# Anpassen der NGINX-Konfiguration mit der tatsächlichen Domain
+NGINX_CONF_PATH="./nginx/nginx.conf" # Pfad zur NGINX-Konfigurationsdatei anpassen
+sudo sed -i "s|ssl_certificate /etc/letsencrypt/live/DEINE_DOMAIN/fullchain.pem;|ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;|g" $NGINX_CONF_PATH
+sudo sed -i "s|ssl_certificate_key /etc/letsencrypt/live/DEINE_DOMAIN/privkey.pem;|ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;|g" $NGINX_CONF_PATH
 
 # Frage nach Supabase URL und Key
 read -p "Bitte geben Sie Ihre Supabase URL ein: " SUPABASE_URL
@@ -41,4 +43,3 @@ pip install gunicorn flask
 # Wechsle Verzeichnis zurück und führe STARTER.sh aus
 cd ..
 ./STARTER.sh
-
