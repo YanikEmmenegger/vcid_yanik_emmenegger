2cdcfe7f5f0380611512a1febbbd9b3cd91f0de6
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-06 10:34:29 +0100

    autoupdater

--- autoupdater/app.py
+++ autoupdater/app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def run_script():
-    os.system("cd ../")
-    os.system("ls")
     os.system("./boot.sh")
     return "update initiated! - site will be down for a few seconds."
 
