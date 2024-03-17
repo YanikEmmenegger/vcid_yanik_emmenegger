4eea3e47247777fe3135dbc1d80db4175a5fd36b
YanikEmmenegger <90186385+YanikEmmenegger@users.noreply.github.com>
2024-03-17 11:25:38 +0100

    Test cases

--- autoupdater/boot.sh
+++ autoupdater/boot.sh
@@ -30,8 +30,6 @@ chmod +x autoupdater/boot.sh
 
 # Tests ausführen
 python -m unittest discover -s tests
-echo "Tests wurden erfolgreich durchgeführt."
-
 # Prüfen ob Tests erfolgreich waren
 if [ $? -ne 0 ]; then
     echo "Tests fehlgeschlagen. Abbruch."
